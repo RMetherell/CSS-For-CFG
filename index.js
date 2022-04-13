9ffa8fd69bb79d83f78dc6890299e0ac44467906
RMetherell <99841069+RMetherell@users.noreply.github.com>
2022-04-13 14:48:19 +0100

    final

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
     if (today > birthday) {
       birthday = dayMonth + nextYear;
     }
-    //end
+    
     
     const countDown = new Date(birthday).getTime(),
         x = setInterval(function() {    
@@ -30,21 +30,21 @@
             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
   
-          //do something later when date is reached
+          
           if (distance < 0) {
             document.getElementById("headline").innerText = "It's my birthday!";
             document.getElementById("countdown").style.display = "none";
             document.getElementById("content").style.display = "block";
             clearInterval(x);
           }
-          //seconds
+          
         }, 0)
     }());
 
 
     var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -55,7 +55,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
